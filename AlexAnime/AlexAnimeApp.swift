65e5d8a8468ee9921777df78aa894fc716347e7b
alexmaxu <12alexmaxu@gmail.com>
2024-04-10 17:15:20 +0200

    Initial Commit

---
+++ AlexAnime/AlexAnimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlexAnimeApp.swift
+//  AlexAnime
+//
+//  Created by Alex  on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AlexAnimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
